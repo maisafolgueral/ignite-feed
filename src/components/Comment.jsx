4c95e9ab312c2265f411d1e11c23a59b50e80925
maisafolgueral <maisa.folgueral@gmail.com>
2024-04-09 18:03:31 -0300

    Updated comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,34 +1,39 @@
-import { ThumbsUp, Trash } from 'phosphor-react';
-import styles from './Comment.module.css';
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
 
 export function Comment() {
-    return (
-        <div className={styles.comment}>
-            <img src="https://github.com/maisafolgueral.png" alt="" />
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/maisafolgueral.png" alt="" />
 
-            <div className={styles.commentBox}>
-                <div className={styles.commentContent}>
-                    <header>
-                        <div className={styles.authorAndTime}>
-                            <strong>Maisa Folgueral</strong>
-                            <time title="02 de abril às 11:20h" dateTime="2024-04-02 11:20:00">Cerca de 1h atrás</time>
-                        </div>
-
-                        <button title="Deletar comentário">
-                            <Trash size={20} />
-                        </button>
-                    </header>
-
-                    <p>Muito bom Devon, parabéns!! 👏👏</p>
-                </div>
-
-                <footer>
-                    <button>
-                        <ThumbsUp />
-                        Aplaudir <span>20</span>
-                    </button>
-                </footer>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Maisa Folgueral</strong>
+              <time
+                title="02 de abril às 11:20h"
+                dateTime="2024-04-02 11:20:00"
+              >
+                Cerca de 1h atrás
+              </time>
             </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
         </div>
-    )
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
