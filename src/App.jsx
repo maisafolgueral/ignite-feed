5d46d41032af8cc5f25c129179137418e116cbbe
maisafolgueral <maisa.folgueral@gmail.com>
2024-04-15 18:52:17 -0300

    Added data structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,87 @@
 // JSX = JavaScript + XML (HTML)
-import { Post } from './components/Post';
-import { Header } from './components/Header';
-import { Sidebar } from './components/Sidebar';
+import { Post } from "./components/Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
-import './global.css';
-import styles from './App.module.css';
+import "./global.css";
+import styles from "./App.module.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/maisafolgueral.png",
+      name: "Maisa Folgueral",
+      role: "Intern",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2024-03-30 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2024-04-05 14:00:00"),
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2024-04-15 11:00:00"),
+  },
+];
 
 export function App() {
   return (
     <div>
-      <Header/>
+      <Header />
 
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-          author="Maisa Folgueral" 
-          content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat velit eius earum amet accusamus eligendi fugiat perferendis reiciendis dolorum iusto soluta labore cupiditate dolorem, magni voluptas, nemo natus id exercitationem!" 
-          />
-          <Post 
-            author="Diego Fernandes" 
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ratione ipsa quod ipsum atque aperiam exercitationem ducimus quis, perferendis a! Magni tempore molestias, ab harum expedita eius quisquam enim aliquam!" 
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
-  )
+  );
 }
-
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,8 @@ import styles from "./Post.module.css";
 import { Comment } from "./Comment";
 import { Avatar } from "./Avatar";
 
-export function Post() {
+export function Post(props) {
+  console.log(props);
   return (
     <article className={styles.post}>
       <header>
@@ -19,20 +20,7 @@ export function Post() {
         </time>
       </header>
 
-      <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          👉 <a href="#">jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
-          <a href="#">#rocketseat</a>{" "}
-        </p>
-      </div>
+      <div className={styles.content}></div>
 
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
