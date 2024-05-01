60d58ff29ca3787a65efa288ecafdaf1e4c481bb
maisafolgueral <maisa.folgueral@gmail.com>
2024-04-30 21:35:24 -0300

    Added like function on comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,10 +3,17 @@ import { Avatar } from "./Avatar";
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -35,9 +42,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
